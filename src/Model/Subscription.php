8d5ad66f3758549f185b85b66af0f65164fe9fb4
Beno!t POLASZEK <bpolaszek@gmail.com>
2020-09-22 21:55:37 +0200

    Add subscription model

---
+++ src/Model/Subscription.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BenTools\MercurePHP\Model;
+
+final class Subscription implements \JsonSerializable
+{
+
+    private string $id;
+    private string $subscriber;
+    private string $topic;
+    private bool $active;
+    private ?array $payload;
+
+    public function __construct(string $id, string $subscriber, string $topic, bool $active, ?array $payload)
+    {
+        $this->id = $id;
+        $this->subscriber = $subscriber;
+        $this->topic = $topic;
+        $this->active = $active;
+        $this->payload = $payload;
+    }
+
+    public function jsonSerialize()
+    {
+        $subscription = [
+            '@context' => 'https://mercure.rocks/',
+            'id' => $this->id,
+            'type' => 'Subscription',
+            'subscriber' => $this->subscriber,
+            'topic' => $this->topic,
+            'active' => $this->active,
+        ];
+
+        if (null !== $this->payload) {
+            $subscription['payload'] = $this->payload;
+        }
+
+        return $subscription;
+    }
+}
